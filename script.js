d701487d5981a760b90093722bc946fdb01ca39d
Divyanshu Kumar <divyanshukumarsingh396@gmail.com>
2026-04-07 19:10:14 +0530

    update

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-let name = prompt("hello");
-console.log(name);
+let num = prompt("Enter the Number :");
+
+if(num % 5 ===0){
+ console.log(num, "Is this multiple of 5");   
+}
+else{
+    console.log(num,"is this not multiple of 5");
+}
