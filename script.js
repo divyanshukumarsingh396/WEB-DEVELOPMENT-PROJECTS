0b8091d289ed86d5c96ab76d4951cf91b1e16ddb
Divyanshu Kumar <divyanshukumarsingh396@gmail.com>
2026-04-08 19:28:28 +0530

    update

--- script.js
+++ script.js
@@ -1,17 +1,12 @@
-let num =44;
+function one(){
+    const username = " div"
 
-if(num >= 80){
-    console.log(num, " A grade wow");
+    function two (){
+        const website = " youtube"
+        console.log(username);
+    }
+   // console.log(website);
+
+    two()
 }
-else if(num >=70 && num<80){
-    console.log(num, " Grade b thik hai");
-}
-else if(num >= 60 && num<70){
-    console.log(num, " grade C  ok hai");
-}
-else if(num >= 50 && num<60){
-    console.log(num, " grade  D low performance");
-}
-else{
-    console.log(num,"fail ho gye bro");
-}
+one()
