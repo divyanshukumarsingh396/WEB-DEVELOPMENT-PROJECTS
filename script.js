1a804b0ff9dd8e1d757e0e017df123fc950388cf
Divyanshu Kumar <divyanshukumarsingh396@gmail.com>
2026-04-10 23:57:32 +0530

    update

--- script.js
+++ script.js
@@ -17,26 +17,26 @@
 // ["","",""]
 // [{},{},{}]
 
-const arr = [1,2,3,4,5]
+// const arr = [1,2,3,4,5]
 
-for( const num of arr ){
- console.log(num);
+// for( const num of arr ){
+//  console.log(num);
  
-}
-const greetings = " Hello  world! "
-for (const greet  of greetings) {
-    console.log(`Each char is ${greet }`);
-}
+// }
+// const greetings = " Hello  world! "
+// for (const greet  of greetings) {
+//     console.log(`Each char is ${greet }`);
+// }
 
-// map
+// // map
 
-const map = new map()
-map.set('IN',"India")
-map.set('USA',"UNITED STATE OF AMERICA")
-map.set('FR',"FRANCE")
-map.set('IN',"India")
+// const map = new map()
+// map.set('IN',"India")
+// map.set('USA',"UNITED STATE OF AMERICA")
+// map.set('FR',"FRANCE")
+// map.set('IN',"India")
 
-console.log(map);
+// console.log(map);
 // for(const key of map)
 // {
 //     console.log(key, ':-', value);
@@ -50,3 +50,17 @@ console.log(map);
 // for (const [key , value] of myObject) {
     
 // }
+
+
+
+
+
+// FOR IN LOOP
+
+const coding = ["js", "ruby ","java","python","cpp"]
+
+
+coding.forEach(function (val){
+    console.log(val);
+    
+})
