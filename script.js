3e3372cd5c1afac35671e2b765bc6cfeb84504e5
Divyanshu Kumar <divyanshukumarsingh396@gmail.com>
2026-04-10 22:26:40 +0530

    for loop create table in 1 to 10

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-for(num=0; num<=100; num++){
-    if(num %2 ===0){
-        console.log("num =",num);
+for(let i = 1; i<=10; i++){
+    console.log(`Outer loop value : ${i}`);
+    for(let j= 0; j<=10; j++){
+        console.log(i + '*' + j +  '= ' + i*j);
         
     }
-   
 }
