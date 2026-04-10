ebe4a37f8ede32d0230bdf68a593aa7ca97d0796
Divyanshu Kumar <divyanshukumarsingh396@gmail.com>
2026-04-10 23:40:51 +0530

    update

--- script.js
+++ script.js
@@ -13,4 +13,40 @@
 //     console.log(element); 
 // }
 
+// for of
+// ["","",""]
+// [{},{},{}]
 
+const arr = [1,2,3,4,5]
+
+for( const num of arr ){
+ console.log(num);
+ 
+}
+const greetings = " Hello  world! "
+for (const greet  of greetings) {
+    console.log(`Each char is ${greet }`);
+}
+
+// map
+
+const map = new map()
+map.set('IN',"India")
+map.set('USA',"UNITED STATE OF AMERICA")
+map.set('FR',"FRANCE")
+map.set('IN',"India")
+
+console.log(map);
+// for(const key of map)
+// {
+//     console.log(key, ':-', value);
+// }
+
+// const myObject = {
+//     'game' : 'NFS',
+//     'game2': 'Spiderman'
+// }
+
+// for (const [key , value] of myObject) {
+    
+// }
