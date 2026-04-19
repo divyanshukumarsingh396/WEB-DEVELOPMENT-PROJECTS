0871507ae57d50ede06550d0b7948491ebc81b2c
Divyanshu Kumar <divyanshukumarsingh396@gmail.com>
2026-04-19 13:42:59 +0530

    update todo web folder

--- script.js
+++ script.js
@@ -255,33 +255,3 @@
 
 //  let para = document.querySelector("p")
 //  para.remove();
- let btn = document.querySelector("#btn");
-
-        btn.addEventListener("click", function() {
-
-            let input = document.querySelector("#task");
-         let task = input.value;
-
-            if (task === "") {
-                alert("Please enter task");
-                 return;
-            }
-
-               let li = document.createElement("li");
-            li.innerText = task;
-
-
-     let deleteBtn = document.createElement("button");
-    deleteBtn.innerText = "Delete";
-
-          
-            deleteBtn.addEventListener("click", function() {
-                li.remove();
-            });
-
-li.appendChild(deleteBtn);
-
-            document.querySelector("#tasklist").appendChild(li);
-
-              input.value = "";
-    });
